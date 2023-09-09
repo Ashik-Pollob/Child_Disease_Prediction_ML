3ccba202d0ca0bcd39d9cedb70416f343dea67ff
S.M. Ashikul Islam Pollob <114177917+Ashik-Pollob@users.noreply.github.com>
2023-09-09 18:01:13 +0600

    Create Split.R

---
+++ Machine_Learning/Split.R
@@ -0,0 +1,86 @@
+---
+Title: "Child Diseases Data Split"
+Author: "S.M. Ashikul Islam Pollob"
+Date: "09-09-2023"
+---
+
+# libraries needed
+library(dplyr)
+library(caret)
+
+#Cross Validation
+train_control <- trainControl(method = "repeatedcv",number = 5, repeats = 25, classProbs = TRUE)
+
+# Select dataset for ARI
+AriData<-Data %>%dplyr:: select(Division, Residence, Wealth, F_Edu, M_Edu, F_Job, M_Job, Household_member, Child_Age, Child_Sex, Breastfeeding, 
+                              Birth_order, Media, Mother_age, BMI, Delivery_place, Age_1stBirth, Toilet, Solid_Fuel, Water, Vit_A, Ceaserean, 
+                              Birth_interval,ch_ari)
+
+# Splitting
+set.seed(11)
+index <- sample(1:nrow(AriData),round(0.80*nrow(AriData)))
+train <- AriData[index,]
+test <- AriData[-index,]
+levels(train$ch_ari) <- c("Yes", "No")
+levels(test$ch_ari) <- c("Yes", "No")
+
+
+
+# Select dataset for Stunting
+StData<-Data %>%dplyr:: select(Division, Residence, Wealth, F_Edu, M_Edu, F_Job, M_Job, Household_member, Child_Age, Child_Sex, Breastfeeding, 
+                              Birth_order, Media, Mother_age, BMI, Delivery_place, Age_1stBirth, Toilet, Solid_Fuel, Water, Vit_A, Ceaserean, 
+                              Birth_interval, nt_ch_stunt)
+
+# Splitting
+set.seed(11)
+index <- sample(1:nrow(StData),round(0.80*nrow(StData)))
+train <- StData[index,]
+test <- StData[-index,]
+levels(train$nt_ch_stunt) <- c("Yes", "No")
+levels(test$nt_ch_stunt) <- c("Yes", "No")
+
+
+
+# Select dataset for Wasting
+WstData<-Data %>%dplyr:: select(Division, Residence, Wealth, F_Edu, M_Edu, F_Job, M_Job, Household_member, Child_Age, Child_Sex, Breastfeeding, 
+                              Birth_order, Media, Mother_age, BMI, Delivery_place, Age_1stBirth, Toilet, Solid_Fuel, Water, Vit_A, Ceaserean, 
+                              Birth_interval,,nt_ch_wast)
+
+# Splitting
+set.seed(11)
+index <- sample(1:nrow(WstData),round(0.80*nrow(WstData)))
+train <- WstData[index,]
+test <- WstData[-index,]
+levels(train$nt_ch_wast) <- c("Yes", "No")
+levels(test$nt_ch_wast) <- c("Yes", "No")
+
+
+
+# Select dataset for Underweight
+UwtData<-Data %>%dplyr:: select(Division, Residence, Wealth, F_Edu, M_Edu, F_Job, M_Job, Household_member, Child_Age, Child_Sex, Breastfeeding, 
+                              Birth_order, Media, Mother_age, BMI, Delivery_place, Age_1stBirth, Toilet, Solid_Fuel, Water, Vit_A, Ceaserean, 
+                              Birth_interval,, nt_ch_underwt)
+
+# Splitting
+set.seed(11)
+index <- sample(1:nrow(UwtData),round(0.80*nrow(UwtData)))
+train <- UwtData[index,]
+test <- UwtData[-index,]
+levels(train$nt_ch_underwt) <- c("Yes", "No")
+levels(test$nt_ch_underwt) <- c("Yes", "No")
+
+
+
+# Select dataset for Diarrhea
+DiarData<-Data %>%dplyr:: select(Division, Residence, Wealth, F_Edu, M_Edu, F_Job, M_Job, Household_member, Child_Age, Child_Sex, Breastfeeding, 
+                              Birth_order, Media, Mother_age, BMI, Delivery_place, Age_1stBirth, Toilet, Solid_Fuel, Water, Vit_A, Ceaserean, 
+                              Birth_interval,,ch_diar)
+
+# Splitting
+set.seed(11)
+index <- sample(1:nrow(DiarData),round(0.80*nrow(DiarData)))
+train <- DiarData[index,]
+test <- DiarData[-index,]
+levels(train$ch_diar) <- c("Yes", "No")
+levels(test$ch_diar) <- c("Yes", "No")
+
