99823be93e5a8bf0919894972303a846fa1c052f
S.M. Ashikul Islam Pollob <114177917+Ashik-Pollob@users.noreply.github.com>
2023-09-16 23:32:50 +0600

    Update ARI.R

--- Machine_Learning/Classification/ARI.R
+++ Machine_Learning/Classification/ARI.R
@@ -1,60 +1,77 @@
+---
+Title: "Child Diseases Classification ARI"
+Author: "S.M. Ashikul Islam Pollob"
+Date: "16-09-2023"
+---
+
+# libraries needed
+library(pROC)
+
+
 # Logistic Regression
 Lr <- train(ch_ari ~ ., 
             data = ari.rose,
             method = "glm",
             family = "binomial",
             trControl = train_control
-)
+           )
+
 lm_pred<-predict(Lr, test, type = "raw")
 confusionMatrix(lm_pred,as.factor(test$ch_ari),mode='everything')
 roc1 <- roc(test$ch_ari, as.numeric(lm_pred))
 auc(roc1)
 
-##Decision Tree
-data.rose <- ROSE(ch_ari ~ ., data = train, seed = 00000)$data
+
+
+#Decision Tree
 Dt <- train(ch_ari ~ .,
-            data = data.rose,
+            data = ari.rose,
             method = "rpart",
             trControl = train_control
-            
-)
+            )
+
 tree_pred <- predict(Dt, test)
 confusionMatrix(tree_pred,test$ch_ari,mode='everything')
 roc2 <- roc(test$ch_ari, as.numeric(tree_pred))
 auc(roc2)
 
+
+
 ##SVM
-data.rose <- ROSE(ch_ari ~ ., data = train, seed = 123)$data
 Svm <- train(ch_ari ~., 
-             data = data.rose, 
+             data = ari.rose, 
              method = "svmLinear", 
              trControl = train_control,  
              preProcess = c("center","scale")
              )
+
 svm_pred <- predict(Svm, test,  type = "raw")
 confusionMatrix(svm_pred,test$ch_ari,mode='everything')
 roc3 <- roc(test$ch_ari, as.numeric(svm_pred))
 auc(roc3)
 
+
+
 ##KNN
-data.rose <- ROSE(ch_ari ~ ., data = train, seed = 123)$data
 Knn <- train(ch_ari ~ .,
-             data = data.rose,
+             data = ari.rose,
              method = 'knn',
              tuneLength = 44,
              trControl = train_control,
              preProc = c("center", "scale"),
              metric = "ROC"
-)
+            )
+
 knn_pred <- predict(Knn, test)
 confusionMatrix(knn_pred,test$ch_ari,mode='everything')
 roc4 <- roc(test$ch_ari, as.numeric(knn_pred))
 auc(roc4)
 
+
+
 ##ANN
-data.rose <- ROSE(ch_ari ~ ., data = train, seed = 1)$data
 Ann <- train(ch_ari~.,
-             data=data.rose,
+             data=ari.rose,
              method = "nnet",
              trControl= train_control,
              preProcess=c("scale","center")
@@ -64,18 +81,17 @@ confusionMatrix(ann_pred,test$ch_ari,mode='everything')
 roc5 <- roc(test$ch_ari, as.numeric(ann_pred))
 auc(roc5)
 
-##RF
-data.rose <- ROSE(ch_ari ~ ., data = train, seed = 123)$data
-tuningGrid <- expand.grid(
-  mtry = c(1, 2, 3, 4, 5))
 
+
+#RF
 Rf <- train(
   ch_ari ~ .,
-  data = data.rose,
+  data = ari.rose,
   method = "rf",
   trControl = train_control,
   ntree = 500
-)
+          )
+
 rf_pred <- predict(Rf, test,  type = "raw")
 confusionMatrix(rf_pred,test$ch_ari,mode='everything')
 roc6 <- roc(test$ch_ari, as.numeric(rf_pred))
